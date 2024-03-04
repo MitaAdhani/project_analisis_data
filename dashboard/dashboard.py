a5c79924cad9d6b38e41dc949031c28f262c1dac
MitaAdhani <160259967+MitaAdhani@users.noreply.github.com>
2024-03-05 04:06:46 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -182,7 +182,7 @@ def load_and_filter_data(filename, start_date, end_date):
     return filtered_df
 
 # Memuat dan menyaring data
-bike_dataFrame = load_and_filter_data("Bike_data.csv", date_range[0], date_range[1])
+bike_dataFrame = load_and_filter_data("dashboard/Bike_data.csv", date_range[0], date_range[1])
 
 # Menampilkan visualisasi
 visualize_bike_count_by_weather(bike_dataFrame)
